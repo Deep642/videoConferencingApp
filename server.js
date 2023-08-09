9f39c8d43e99d863ec508c96942ab478f1de083a
Dipayan Mandal <dipayan16082001@gmail.com>
2023-08-09 10:30:44 +0530

    changes in Server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+const server = http.createServer(app);
 const { v4: uuidv4 } = require("uuid");
+const cors =  require("cors");
+app.use(cors());
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
   cors: {
